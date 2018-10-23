4359644b81e82b25df284597ddc696b50e08ccc8
Mykenzie Rogers <mykenzierogers@gmail.com>
2018-10-23 00:50:47 -0500

    finished assignment

--- assessment.js
+++ assessment.js
@@ -17,11 +17,21 @@ let me = "My name is " + myName + " and my favorite number is " + faveNum;
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
 // CODE HERE
-let trueFaveNum = faveNum === 7 ? true : false;
+let trueFaveNum;
+if (faveNum === 7) {
+  trueFaveNum = true;
+} else {
+  trueFaveNum = false;
+}
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 // CODE HERE
-let notHakeem = myName === "Hakeem Olajuwon" ? true : false;
+let notHakeem;
+if (myName != "Hakeem Olajuwon") {
+  notHakeem = true;
+} else {
+  notHakeem = false;
+}
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
@@ -36,9 +46,9 @@ var colorResponse;
 var responseCreator = function(param){
   // CODE HERE
     if(param === "green") {
-      colorResponse = "Green is okay.";
+      colorResponse = "Green is okay";
     } else if (param === "red") {
-      colorResponse = "I heart red.";
+      colorResponse = "I heart red";
     } else {
       colorResponse = "What is your favorite color?";
     }
@@ -55,5 +65,5 @@ let sum = function(param1) {
 
 // CODE HERE
 let greeter = function(name, number) {
-  return "Hi! I am " + name + " and I am " + number + " years old!";
+  return "Hi! I am " + name + " and I am " + number + " years old";
 }
